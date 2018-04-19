6657596bd5977896617ffd9bc8d0eddcfb9fe21d
lvfx00 <lvfx00@gmail.com>
2018-04-19 13:31:31 +0000

    added B matrix scattering

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -4,7 +4,7 @@
 #include "assert.h"
 
 #define n1 32
-#define n2 4
+#define n2 6
 #define n3 16
 
 int main(int argc, char **argv) {
@@ -34,30 +34,6 @@ int main(int argc, char **argv) {
     int comm_rank_y = coords[0];
     int comm_rank_x = coords[1];
 
-    // create matrxes in (0, 0) process
-    double *A;
-    double *B;
-
-    if(comm_rank_x == 0 && comm_size_y == 0) {
-        A = malloc(n1 * n2 * sizeof(double));
-        assert(A != NULL);
-        // fill the matrix
-        for(int i = 0; i < n1; ++i) {
-            for(int j = 0; j < n2; ++j) {
-                A[i * n2 + j] = 1000 * i + j;
-            }
-        }
-
-        B = malloc(n2 * n3 * sizeof(double));
-        assert(B != NULL);
-        // fill the matrix
-        for(int i = 0; i < n2; ++i) {
-            for(int j = 0; j < n3; ++j) {
-                B[i * n3 + j] = 1000 * i + j;
-            }
-        }
-    }
-
     // split comm2d into horizontal lines communicators
     int color = comm_rank_y;
     int key = comm_rank_x;
@@ -83,24 +59,33 @@ int main(int argc, char **argv) {
     MPI_Comm_size(col_comm, &col_size);
 
     //printf("CARD X/Y: %d/%d \t ROW RANK/SIZE: %d/%d\n", comm_rank_x, comm_rank_y, row_rank, row_size);
-    printf("CARD X/Y: %d/%d \t COL RANK/SIZE: %d/%d\n", comm_rank_x, comm_rank_y, col_rank, col_size);
+    //printf("CARD X/Y: %d/%d \t COL RANK/SIZE: %d/%d\n", comm_rank_x, comm_rank_y, col_rank, col_size);
     
-    /*
+    //////////////////////////////////////////////////////////////////////////////
+    // create A matrix in (0, 0) process
+    double *A;
+    if(comm_rank_x == 0 && comm_rank_y == 0) {
+        A = (double*)malloc(n1 * n2 * sizeof(double));
+        assert(A != NULL);
+        // fill the matrix
+        for(int i = 0; i < n1; ++i) {
+            for(int j = 0; j < n2; ++j) {
+                A[i * n2 + j] = 1000 * i + j;
+            }
+        }
+    }
+    
     // create type for A matrix row
     MPI_Datatype row_type;
-    MPI_Type_contiguous(n2, MPI_DOUBLE, &row_type); // n2 - num of elements in row
+    MPI_Type_contiguous(n2, MPI_DOUBLE, &row_type);
     MPI_Type_commit(&row_type);
 
-
-    ///////////////////////////////////////////////////////////////////////
     // number of rows per one processor
     int row_num = n1 / comm_size_y; 
 
     // space for row_num lines with n2 elements
     double *A_part = (double*)malloc(row_num * n2 * sizeof(double));
     assert(A_part != NULL);
-    
-    // scatter A matrix rows between x=0 processes (first column)
         
     // number of sending elements for each process
     int * sendcounts = (int *)malloc(comm_size_y * sizeof(int));
@@ -117,20 +102,71 @@ int main(int argc, char **argv) {
         displs[i] = displs[i - 1] + row_num;
     }
 
+    // scatter A matrix rows between x=0 processes (first column)
     if(comm_rank_x == 0) {
         MPI_Scatterv(A, sendcounts, displs, row_type, A_part, row_num, row_type, 0, col_comm); 
     }
-
+        //// both works too
+        //MPI_Scatter(A, row_num * n2, MPI_DOUBLE, A_part, row_num * n2, MPI_DOUBLE, 0, col_comm);
+        //MPI_Scatter(A, row_num, row_type, A_part, row_num, row_type, 0, col_comm);
+        
+    /*
     if(comm_rank_x == 0 && comm_rank_y == 0) {
-        for(int i = 0; i < row_num * n2; ++i)
-        printf("%lf   ", A_part[i]);
+        printf("%d\n", row_num);
+        for(int i = 0; i < row_num; ++i) {
+            for(int j = 0; j < n2; ++j) {
+                printf("%lf   ", A_part[i * n2 + j]);
+            }
+            printf("\n");
+        }
     }
+    */
 
-    MPI_Datatype col_type;
-    MPI_Type_vector(n2, 1, n1, MPI_DOUBLE, &col_type);
+    // broadcast A_parts through process rows
+    MPI_Bcast(A_part, row_num, row_type, 0, row_comm);
+
+    ////////////////////////////////////////////////////////////////////////////
+    // create B matrix in (0, 0) process
+
+    double *B;
+    if(comm_rank_x == 0 && comm_rank_y == 0) {
+        B = malloc(n2 * n3 * sizeof(double));
+        assert(B != NULL);
+        // fill the matrix
+        for(int i = 0; i < n2; ++i) {
+            for(int j = 0; j < n3; ++j) {
+                B[i * n3 + j] = 1000 * i + j;
+            }
+        }
+    }
+    
+    // number of rows per one processor
+    int col_num = n3 / comm_size_x; 
+
+    // create type for B matrix column
+    MPI_Datatype temp_type, col_type;
+    MPI_Type_vector(n2, col_num, n3, MPI_DOUBLE, &temp_type);
+    MPI_Type_create_resized(temp_type, 0, sizeof(double) * col_num, &col_type);
     MPI_Type_commit(&col_type);
 
-    */
+    // space for row_num lines with n2 elements
+    double *B_part = (double*)malloc(col_num * n2 * sizeof(double));
+    assert(B_part != NULL);
+        
+    // scatter A matrix rows between x=0 processes (first column)
+    if(comm_rank_y == 0) {
+        MPI_Scatter(B, 1, col_type, B_part, col_num, row_type, 0, row_comm);
+    }
+
+    if(comm_rank_x == 3 && comm_rank_y == 1) {
+        printf("%d\n", col_num);
+        for(int i = 0; i < n2; ++i) {
+            for(int j = 0; j < col_num; ++j) {
+                printf("%lf   ", B_part[i * col_num + j]);
+            }
+            printf("\n");
+        }
+    }
 
     MPI_Comm_free(&row_comm);
     MPI_Comm_free(&col_comm);
